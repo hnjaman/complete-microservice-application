78003e7ec3a47d374fdc4a80d7bed88fab48261b
hnjaman <hnjsujon.mbstu@gmail.com>
2020-02-18 08:47:06 +0600

    api gateway

---
+++ api-gateway/src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.hnj.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+//@EnableEurekaClient        // It acts as a eureka client
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
