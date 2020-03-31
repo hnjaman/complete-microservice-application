458b524ee232d9dd295df3655257fc4991a69555
hnjaman <hnjsujon.mbstu@gmail.com>
2020-03-31 19:04:32 +0600

    zuul routing, load balance, hystrix with all service up & running

--- api-gateway/src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
@@ -3,11 +3,11 @@ package com.hnj.apigateway;
 import com.hnj.apigateway.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 @RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 @SpringBootApplication
--- product-service/src/main/java/com/hnj/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/hnj/product/ProductServiceApplication.java
@@ -2,9 +2,9 @@ package com.hnj.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
     public static void main(String[] args) {
