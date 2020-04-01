fa3b1fc4b76f1db9e55c72e9d5cb0e4d998264ed
hnjaman <hnjsujon.mbstu@gmail.com>
2020-04-02 00:04:54 +0600

    remove unwanted code like multiplication, ribbon hosts and offer event key changed

--- api-gateway/src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
@@ -1,18 +1,14 @@
 package com.hnj.apigateway;
 
-import com.hnj.apigateway.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
-@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 @SpringBootApplication
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
